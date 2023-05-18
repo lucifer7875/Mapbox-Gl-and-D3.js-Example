4ab81fcf97648c6379cee3d5a77cc74870f9d48e
Hardiksinh Rajput <98508248+lucifer7875@users.noreply.github.com>
2023-05-18 12:17:40 +0530

    First Commit

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
-<h1>Mapbox-Gl-and-D3.js-Example</h1>
-    &nbsp;
-    <h6>Follow steps</h6>
-    <ul>
-      <li>Step 1 :- npm i</li>
-      <li>Step 2 :- npm start</li>
-    </ul>
-    <h4 style="color: blue;">Info</h4>
-    <p>
-      you have node 16 vesion if you are doing
-      <span style="text-decoration: underline;">npm i</span>
-    </p>
+Data overlay example
+---
+
+Add a data overlay to a map via React and use controls to visualize it differently.
+
+![Demo gif](https://i.imgur.com/FiqNRfZ.gif)
+
+### Run it
+
+    git clone https://github.com/mapbox/mapbox-react-examples.git
+
+    cd mapbox-react-examples/data-overlay
+    npm install
+    npm start
+
+    open http://localhost:3000/
+
+Runs the app from http://localhost:3000
+
+Data provided by [Natural Earth](http://www.naturalearthdata.com/).
---
+++ src/Map.js
@@ -0,0 +1,163 @@
+import React, { useRef, useEffect, useState } from 'react'
+import mapboxgl from 'mapbox-gl'
+import Legend from './components/Legend'
+import Optionsfield from './components/Optionsfield'
+import './Map.css'
+import data from './data.json'
+import { BarPlot } from './components/BarPlot/BarPlot'
+import { color_breaks, oregon_county_pop_data } from './components/Data/data'
+
+// mapboxgl.accessToken =
+//   'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA'
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoid2lsbGNhcnRlciIsImEiOiJjamV4b2g3Z2ExOGF4MzFwN3R1dHJ3d2J4In0.Ti-hnuBH8W4bHn7k6GCpGw'
+
+const Map = () => {
+  const [selectedId, setSelectedId] = useState(null)
+  const [statefulMap, setStatefulMap] = useState(null)
+
+  const options = [
+    {
+      name: 'Population',
+      description: 'Estimated total population',
+      property: 'pop_est',
+      stops: [
+        [0, '#f8d5cc'],
+        [1000000, '#f4bfb6'],
+        [5000000, '#f1a8a5'],
+        [10000000, '#ee8f9a'],
+        [50000000, '#ec739b'],
+        [100000000, '#dd5ca8'],
+        [250000000, '#c44cc0'],
+        [500000000, '#9f43d7'],
+        [1000000000, '#6e40e6'],
+      ],
+    },
+    {
+      name: 'GDP',
+      description: 'Estimate total GDP in millions of dollars',
+      property: 'gdp_md_est',
+      stops: [
+        [0, '#f8d5cc'],
+        [1000, '#f4bfb6'],
+        [5000, '#f1a8a5'],
+        [10000, '#ee8f9a'],
+        [50000, '#ec739b'],
+        [100000, '#dd5ca8'],
+        [250000, '#c44cc0'],
+        [5000000, '#9f43d7'],
+        [10000000, '#6e40e6'],
+      ],
+    },
+  ]
+  const mapContainerRef = useRef(null)
+  const [active, setActive] = useState(options[0])
+  const [map, setMap] = useState(null)
+
+  // Initialize map when component mounts
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      selectedId,
+      container: mapContainerRef.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [5, 34],
+      zoom: 1.5,
+    })
+
+    map.on('load', () => {
+      map.addSource('countries', {
+        type: 'geojson',
+        data,
+      })
+
+      map.setLayoutProperty('country-label', 'text-field', [
+        'format',
+        ['get', 'name_en'],
+        { 'font-scale': 1.2 },
+        '\n',
+        {},
+        ['get', 'name'],
+        {
+          'font-scale': 0.8,
+          'text-font': [
+            'literal',
+            ['DIN Offc Pro Italic', 'Arial Unicode MS Regular'],
+          ],
+        },
+      ])
+
+      map.addLayer(
+        {
+          id: 'countries',
+          type: 'fill',
+          source: 'countries',
+        },
+        'country-label',
+      )
+
+      map.setPaintProperty('countries', 'fill-color', {
+        property: active.property,
+        stops: active.stops,
+      })
+
+      setMap(map)
+      setStatefulMap(map)
+    })
+
+    console.log(
+      '106',
+      `selectedId is not null, highlight selected country name: ${selectedId}`,
+    )
+    // Clean up on unmount
+    return () => map.remove()
+  }, [selectedId])
+
+  useEffect(() => {
+    paint()
+  }, [active])
+
+  const paint = () => {
+    if (map) {
+      map.setPaintProperty('countries', 'fill-color', {
+        property: active.property,
+        stops: active.stops,
+      })
+    }
+  }
+
+  const changeState = (i) => {
+    setActive(options[i])
+    map.setPaintProperty('countries', 'fill-color', {
+      property: active.property,
+      stops: active.stops,
+    })
+  }
+
+  return (
+    <div>
+      <div ref={mapContainerRef} className="map-container" />
+      {/* <Legend active={active} stops={active.stops} /> */}
+      <Optionsfield
+        options={options}
+        property={active.property}
+        changeState={changeState}
+      />
+
+      <BarPlot
+        data={oregon_county_pop_data()}
+        svgWidth={450}
+        svgHeight={275}
+        itemDelay={200}
+        onSelectItem={setSelectedId}
+        colorBreaks={color_breaks()}
+        highlightLineColor={{ rgba: [255, 102, 0, 1] }}
+        tiltXLabels={true}
+        visualizationTitle="Counties Population "
+        leftAxisTitle="Count"
+        bottomAxisTitle="County"
+      />
+    </div>
+  )
+}
+
+export default Map
